84fa9f33218e26f4aa2d75ae5502272f18d34a59
Volodymyr Korniychuk <korniychuk49@gmail.com>
2023-04-06 18:58:14 +0300

    commit

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+
+export async function fetchImages() {
+  const KEY = '35102712-8af4928d6ff69b4cac4795091';
+  const BASE_URL = 'https://pixabay.com/api/';
+
+  const response = await axios.get(
+    `${BASE_URL}?key=${KEY}&q=image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+  );
+  const images = await response.json();
+  return images;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import Notiflix from 'notiflix';
+import { fetchImages } from './fetchImages';
+
+const formEl = document.querySelector('.search-form');
+const divEl = document.querySelector('.gallery');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+  const inputValue = event.currentTarget.elements.searchQuery.value;
+
+  try {
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function createImagesListMarcup(imageCard) {
+  const marcup = imageCard
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` <div class="photo-card">
+                <a>
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                </a>
+               
+                  <div class="info">
+                     <p class="info-item">
+                        <b>${likes}</b>
+                     </p>
+
+                     <p class="info-item">
+                        <b>${views}</b>
+                     </p>
+
+                     <p class="info-item">
+                        <b>${comments}</b>
+                     </p>
+
+                     <p class="info-item">
+                        <b>${downloads}</b>
+                     </p>
+                  </div>
+            </div>`;
+      }
+    )
+    .join('');
+
+  divEl.insertAdjacentHTML('beforeend', marcup);
+}
+
+// function resetInput() {
+//   formEl.innerHTML = '';
+// }
